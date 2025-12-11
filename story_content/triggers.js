6a7ff5276a710c439685047e07ad33a97350ff83
afdalmukhair999-art <afdalmukhair999@gmail.com>
2025-12-12 01:52:50 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,63 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5sBMuQ1NJbN":
+        Script1();
+        break;
+      case "6d5EsINyI6I":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6S9LKuxwQuR');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5rXexEwife4';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6GCA5Lnqj6p');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6D6JKctCoOx';
+const growAmount = 0.2;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
